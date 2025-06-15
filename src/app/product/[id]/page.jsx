b040539d7bc07b59fc26562d6ec7afdaf36c90ec
AFNAAN KHAN <afnaank01@gmail.com>
2025-06-16 04:16:58 +0530

    product Page is simply ready.

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,5 +1,8 @@
 import Navbar from "@/components/navbar/App";
 import 'remixicon/fonts/remixicon.css'
+import { Button } from "@/components/ui/button";
+import Products from "@/sections/products/App"; 
+import Footer from "@/components/footer/App";
 
 
 // Mock product data (replace with real fetch later)
@@ -60,8 +63,8 @@ const ProductPage = ({ params }) => {
 
             {/* image changer */}
             <div className="w-full flex justify-center items-center gap-2 mt-4">
-               <span className="text-4xl"><i class="ri-arrow-left-circle-line"></i></span>
-               <span className="text-4xl"><i class="ri-arrow-right-circle-line"></i></span>
+              <span className="text-4xl"><i class="ri-arrow-left-circle-line"></i></span>
+              <span className="text-4xl"><i class="ri-arrow-right-circle-line"></i></span>
             </div>
 
           </div>
@@ -69,16 +72,50 @@ const ProductPage = ({ params }) => {
           {/* Product Details */}
           <div>
 
-            <p className="text-blue-600 text-xl font-semibold mt-2">
-              ₹{products.price}
-            </p>
+            {/* Product Pricing */}
+            <div className="flex gap-1 items-center">
+              <p className="text-blue-600 font-bold text-3xl">₹1200</p>
+              <div className="flex gap-1">
+                <p>M.R.P:</p>
+                <p className="text-gray-600 line-through">₹3000</p>
+              </div>
+            </div>
 
-            <button className="mt-6 bg-blue-600 text-white px-6 py-3 rounded hover:bg-blue-700 transition-all text-sm">
-              Add to Cart
-            </button>
+            {/* Product Quantity Picker */}
+            <div className="mt-4">
+              <label htmlFor="quantity" className="block text-gray-700 mb-2">
+                Quantity:
+              </label>
+              <input
+                type="number"
+                id="quantity"
+                name="quantity"
+                min="1"
+                defaultValue="1"
+                className="w-20 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              </div>
+
+            <div className="border-b border-gray-400 pb-4">
+              <Button variant={"addcart"}>
+                Add to cart
+              </Button>
+              <Button variant={"buynow"}>
+                Buy Now
+              </Button>
+            </div>
+
+            <div className="mt-6 border-b border-gray-400 pb-4">
+              <h2 className="text-xl">About This Product:</h2>
+            </div>
+
           </div>
         </div>
       </section>
+
+      <Products text={"Related Products"} />
+
+      <Footer />
     </div>
 
   );
