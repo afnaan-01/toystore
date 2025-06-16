48a9f6e677ab0364e6ee90b525bb41f351135818
AFNAAN KHAN <afnaank01@gmail.com>
2025-06-16 19:53:35 +0530

    Testing

--- src/components/card/App.jsx
+++ src/components/card/App.jsx
@@ -18,24 +18,25 @@ const App = ({ id, title, imageUrl, price }) => {
                         className="w-full h-full object-cover"
                     />
                 </div>
-
-                <div className="p-4">
-                    <h3 className="text-md font-semibold text-gray-800 truncate">{title}</h3>
-                    <div className="flex gap-1 items-center">
-                        <p className="text-blue-600 font-bold text-2xl">₹{price}</p>
-                        <div className="flex gap-1">
-                            <p>M.R.P:</p>
-                            <p className="text-gray-600 line-through">₹3000</p>
-                        </div>
-                    </div>
-                    <Button variant={"addcart"}>
-                        Add to cart
-                    </Button>
-                    <Button variant="buynow" onClick={() => router.push(`/checkout/${id}/1}`)}>
-                        Buy Now
-                    </Button>
-                </div>
             </Link>
+
+            <div className="p-4">
+                <h3 className="text-md font-semibold text-gray-800 truncate">{title}</h3>
+                <div className="flex gap-1 items-center">
+                    <p className="text-blue-600 font-bold text-2xl">₹{price}</p>
+                    <div className="flex gap-1">
+                        <p>M.R.P:</p>
+                        <p className="text-gray-600 line-through">₹3000</p>
+                    </div>
+                </div>
+                <Button variant={"addcart"}>
+                    Add to cart
+                </Button>
+                <Button variant="buynow" onClick={() => router.push(`/checkout/${id}/1}`)}>
+                    Buy Now
+                </Button>
+            </div>
+
         </div>
     );
 };
