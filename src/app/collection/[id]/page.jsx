3e04e6e8f040b4deae2b613dc2d63f9900018f47
AFNAAN KHAN <afnaank01@gmail.com>
2025-06-17 12:15:22 +0530

    Bug or Warning solved for key attribute

--- src/app/collection/[id]/page.jsx
+++ src/app/collection/[id]/page.jsx
@@ -64,7 +64,7 @@ export default function ShopPage({params}) {
           <div className="md:col-span-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {sortedProducts.map((product) => (
               <ProductCard
-                key={product.id}
+                key={Math.random()}
                 id={product.id}
                 title={product.title}
                 imageUrl={product.imageUrl}
