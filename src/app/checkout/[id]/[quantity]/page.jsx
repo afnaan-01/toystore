07e8279f86e170ca3a85c1a981783464ec6c3003
AFNAAN KHAN <afnaank01@gmail.com>
2025-06-23 11:15:35 +0530

    Checkout page updated for functionality. Product fetched based on id and order object created with order details.

--- src/app/checkout/[id]/[quantity]/page.jsx
+++ src/app/checkout/[id]/[quantity]/page.jsx
@@ -1,43 +1,49 @@
 "use client";
 
-import { use, useState } from "react";
-import { useParams } from "next/navigation";
+import { useState, useEffect, use } from "react";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
-export default function CheckoutPage() {
+export default function CheckoutPage({ params }) {
 
-  console.log("ok");
-  
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    address: "",
-    city: "",
-    zip: "",
-    phone: "",
-    payment: "cod",
-    coupon: "",
-  });
+  const { id, quantity } = use(params);
 
+  const { register, handleSubmit, reset } = useForm();
+  const [orderProduct, setOrderProduct] = useState();
   const [placingOrder, setPlacingOrder] = useState(false);
 
-  const cartItems = [
-    { id: 1, title: "RC Car", price: 1299, quantity: 1 },
-    { id: 2, title: "Robot Kit", price: 2499, quantity: 2 },
-  ];
 
-  const total = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  //Fetch Product
+  useEffect(() => {
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-  };
+    const fetchProducts = async () => {
+      try {
+        console.log(id);
+        const res = await axios.get(`/api/fatch-single-product/${id}`);
+        console.log('Fetched data:', res.data.product);
+        setOrderProduct(res.data.product || {});
+      } catch (err) {
+        console.error('Error fetching product:', err);
+      }
+    };
+    fetchProducts();
+  }, []);
 
-  const placeOrder = () => {
+  // const total = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+  const onSubmit = (data) => {
     setPlacingOrder(true);
+    console.log("Form Data:", data);
+
+    const order = {
+      customer: data,
+      product: orderProduct._id
+    }
+
     setTimeout(() => {
       alert("✅ Order placed successfully!");
       setPlacingOrder(false);
+      console.log("Order Data:", order);
     }, 2000);
   };
 
@@ -45,20 +51,20 @@ export default function CheckoutPage() {
     <section className="max-w-6xl mx-auto px-4 py-10">
       <h1 className="text-3xl font-bold mb-8 text-center">Checkout</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Shipping Details */}
         <div className="bg-white p-6 rounded-xl shadow">
           <h2 className="text-xl font-semibold mb-4">Shipping Details</h2>
 
           <div className="space-y-4">
-            <input name="name" onChange={handleChange} placeholder="Full Name" className="w-full border rounded p-2" required />
-            <input name="email" onChange={handleChange} placeholder="Email" className="w-full border rounded p-2" required />
-            <input name="address" onChange={handleChange} placeholder="Address" className="w-full border rounded p-2" required />
+            <input {...register("name", { required: true })} placeholder="Full Name" className="w-full border rounded p-2" required />
+            <input type="email" {...register("email", { required: true })} placeholder="Email" className="w-full border rounded p-2" required />
+            <input {...register("address", { required: true })} placeholder="Address" className="w-full border rounded p-2" required />
             <div className="flex gap-4">
-              <input name="city" onChange={handleChange} placeholder="City" className="w-full border rounded p-2" required />
-              <input name="zip" onChange={handleChange} placeholder="Postal Code" className="w-full border rounded p-2" required />
+              <input {...register("city", { required: true })} placeholder="City" className="w-full border rounded p-2" required />
+              <input {...register("zip", { required: true })} placeholder="Postal Code" className="w-full border rounded p-2" required />
             </div>
-            <input name="phone" onChange={handleChange} placeholder="Phone" className="w-full border rounded p-2" required />
+            <input {...register("phone", { required: true })} placeholder="Phone" className="w-full border rounded p-2" required />
           </div>
 
           {/* Payment Options */}
@@ -66,11 +72,11 @@ export default function CheckoutPage() {
             <h3 className="font-semibold text-gray-700 mb-2">Payment Method</h3>
             <div className="space-y-2">
               <label className="flex items-center gap-2">
-                <input type="radio" name="payment" value="cod" checked={form.payment === "cod"} onChange={handleChange} />
+                <input type="radio" value="cod" {...register("payment")} defaultChecked />
                 Cash on Delivery (COD)
               </label>
               <label className="flex items-center gap-2">
-                <input type="radio" name="payment" value="razorpay" checked={form.payment === "razorpay"} onChange={handleChange} />
+                <input type="radio" value="razorpay" {...register("payment")} />
                 Razorpay (Online)
               </label>
             </div>
@@ -80,10 +86,8 @@ export default function CheckoutPage() {
           <div className="mt-6">
             <input
               type="text"
-              name="coupon"
-              value={form.coupon}
-              onChange={handleChange}
               placeholder="Enter Promo Code"
+              {...register("coupon")}
               className="w-full border rounded p-2"
             />
           </div>
@@ -93,28 +97,29 @@ export default function CheckoutPage() {
         <div className="bg-white p-6 rounded-xl shadow h-fit">
           <h2 className="text-xl font-semibold mb-4">Order Summary</h2>
 
-          <div className="divide-y divide-gray-200">
-            {cartItems.map((item) => (
-              <div key={item.id} className="py-3 flex justify-between text-sm">
-                <span>{item.title} × {item.quantity}</span>
-                <span>₹{item.price * item.quantity}</span>
+          {orderProduct && (
+            <div className="divide-y divide-gray-200">
+              <div key={orderProduct.id} className="py-3 flex justify-between items-center text-sm">
+                <span><img src={orderProduct.images[0]} alt={orderProduct.productName} className="h-10" /></span>
+                <span>{orderProduct.productName} × {quantity}</span>
+                <span>₹ {orderProduct.finalPrice * quantity}</span>
               </div>
-            ))}
-            <div className="py-3 flex justify-between font-semibold">
-              <span>Total</span>
-              <span>₹{total}</span>
+              <div className="py-3 flex justify-between font-semibold">
+                <span>Total</span>
+                {/* <span>₹{total}</span> */}
+              </div>
             </div>
-          </div>
+          )}
 
           <button
-            onClick={placeOrder}
+            type="submit"
             className="w-full bg-blue-600 text-white py-3 mt-6 rounded hover:bg-blue-700 transition-all text-sm disabled:opacity-50"
             disabled={placingOrder}
           >
             {placingOrder ? "Placing Order..." : "Place Order"}
           </button>
         </div>
-      </div>
+      </form>
     </section>
   );
 }
