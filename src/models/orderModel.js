6bb352c5e06d56ab0dc2aae1a7185ae6ab08ae8d
sufiyan10-1 <134135723+sufiyan10-1@users.noreply.github.com>
2025-06-25 13:21:35 +0530

    place order api and model update

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,8 +1,50 @@
  
 import mongoose  from "mongoose";
  
+const userSchema = new mongoose.Schema({
+    id: {
+      type: String,
+      default: null,
+    },
+    name: {
+      type: String,
+      default: null,
+    },
+     email: {
+      type: String,
+      default: null,
+    }
+})
+
+const productSchema = new mongoose.Schema({
+    productId: {
+      type: String,
+      required: [true, "product id is required"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "product name is required"],
+    } 
+})
+
 const addressSchema = new mongoose.Schema({
-    address:{
+   fullName:{
+    type: String,
+    required: [true, "full name id is required"],
+   },
+   email:{
+    type: String,
+    default: null
+   },
+   countryCode:{
+    type: String,
+    default: "+91"
+   },
+   phoneNo:{
+    type: Number,
+    required: [true, "phoneNo is required"],
+   }, 
+   address:{
      type: String,
      required: [true, "address is required"],
    },
@@ -14,33 +56,50 @@ const addressSchema = new mongoose.Schema({
     type: String,
     required: [true, "state is required"],
    },
-    country:{
+   country:{
     type: String,
     default: "India"
    },
-
    pinCode:{
     type: Number,
     required: [true, "pinCode is required"],  
    },
-    countryCode:{
-    type: String,
-    default: "+91"
-   },
-   phoneNo:{
-    type: Number,
-    required: [true, "phoneNo is required"],
-   }, 
    landmark:{
     type: String,
     default: null
    },
-   
+})
+ 
+const paymentInfoSchema = new mongoose.Schema({
+     paymentId: {
+      type: String,
+      default: null
+    },
+    paymentstatus: {
+      type: String,
+      default: null
+    } 
+})
 
-  })
- 
- 
- 
+const pricingSchema = new mongoose.Schema({
+     productAmount: {
+      type: Number,
+      required: [true, "product Amount is required"],
+    },
+    shippingCharges: {
+      type: Number,
+      default: 0
+    },
+     tax: {
+      type: Number,
+      default: 0
+    },
+     totalAmount: {
+      type: Number,
+      default: 0
+    }
+})
+
 
 const OrderSchema = new mongoose.Schema({
    orderId: {
@@ -48,38 +107,38 @@ const OrderSchema = new mongoose.Schema({
     required: [true, "order id is required"],
     unique: true,
    },
-   fullName: {
-    type:  String,
-    required: [true, "full name is required"],
-   },
-   email: {
-    type:  String,
-    required: [true, "email is required"],
-   },
-  address: {
-    type: [addressSchema],
-  },
-  paymentMethod: {
+   user: userSchema,
+
+   shippingAddress: addressSchema,
+  
+   product: productSchema,
+ 
+
+   paymentMethod: {
     type:  String,
     required: [true, "Payment Method is required"],
-    default: "Cash on Delivery"
-  },
-   promoCode: {
-    type:  String,
+    default: "cod"
+   },
+   paymentInfo: paymentInfoSchema, 
+   
+   pricing: pricingSchema,
+
+   orderStatus: {
+    type: String,
     default: null
-  },
-  productId: {
-    type:  String,
-    required: [true, "productId Method is required"],
-  },
-  quantity: {
-    type:  Number,
-    required: [true, "quantity Method is required"],
-  },
-  totalAmount: {
-     type:  Number,
-    required: [true, "quantity Method is required"],
-  }
+   },
+   isPaid: {
+    type: Boolean,
+    default: false,
+   },
+   orderAt: {
+    type: Date,
+     required: [true, "orderAt id is required"],
+   },
+   deliveredAt: {
+    type: Date,
+    default: null,
+   }
 
 })
 const orderModel = (mongoose.models.orderModel) || mongoose.model('orderModel', OrderSchema);
