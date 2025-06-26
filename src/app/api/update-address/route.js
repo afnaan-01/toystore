08381b3c8beb99f125561bba2c09791b8464ab90
sufiyan10-1 <134135723+sufiyan10-1@users.noreply.github.com>
2025-06-26 10:16:41 +0530

    update address api added

---
+++ src/app/api/update-address/route.js
@@ -0,0 +1,72 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+
+
+export async function POST(request) {
+     await dbConnect();
+    //  const session = await getServerSession(authOptions);
+     
+    //   if (!session || !session.user) {
+    //     return NextResponse.json({ 
+    //         success: false, 
+    //         message: "Unauthorized" 
+    //     }, { status: 401 });
+    //   }
+    
+       
+      //const userId = session._id;  
+   try {
+      const {
+        userId,
+        addressId, 
+        address, 
+        city, 
+        state, 
+        landmark, 
+        phoneNo, 
+        pinCode } = await request.json();
+
+        const user = await UserModel.findById(userId)
+        if (!user) {
+            return NextResponse.json({
+                success: false,
+                message: 'There are no User present',
+            }, { status: 400 });
+        }
+
+
+        const updatedUser = await UserModel.findOneAndUpdate(
+      { _id: userId, "addresses._id": addressId },
+      {
+        $set: {
+          "addresses.$.address": address,
+          "addresses.$.city": city,
+          "addresses.$.state": state,
+          "addresses.$.landmark": landmark,
+          "addresses.$.phoneNo": phoneNo,
+          "addresses.$.pinCode": pinCode,
+        },
+      },
+      { new: true }
+    );
+   if(updatedUser){
+    return NextResponse.json({
+      success: true,
+      message: "Address updated successfully",
+      user: updatedUser,
+    });
+}
+   } catch (error) {
+     console.error("Error updating address:", error);
+    return NextResponse.json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    }, { status: 500 });
+  
+   }    
+
+}
